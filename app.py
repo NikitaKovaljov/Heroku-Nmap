12a3f6f3adfa647547194c42aab4ae1a43e5cf8f
Nikita Kovaljov <nikita.kovaljov@gmail.com>
2021-11-26 13:41:26 +0200

    Less go

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask,render_template, request
+import flask
+#import nmap3
+  
+app = Flask(__name__)
+
+  
+@app.route("/")
+def pages():
+    return render_template('/templates/index')
+
+@app.route('/nmap.html', methods = ['POST','GET'])
+def nmap():
+    if request.method == "POST":
+        nmap_request = request.form.get("nmap_request")
+        if len(nmap_request) == 0:
+            return "Problems with Request"
+        else:
+            return nmap_request
+  
+if __name__ == "__main__":
+  app.run()
