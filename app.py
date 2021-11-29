c0489ad9ab3cfb8154b278e1bf56e9f9830a10c3
Nikita Kovaljov <nikita.kovaljov@gmail.com>
2021-11-29 21:43:50 +0200

    asd

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ def pages():
 @app.route('/nmap', methods = ['POST','GET'])
 def nmap():
     if request.method == "POST":
-        nmap_args = request.form.get("args")
-        nmap_host = request.form.get("host")
+        #nmap_args = request.form.get("args")
+        #nmap_host = request.form.get("host")
 
         nmap_host = "scanme.nmap.org"
         nmap_args = "-v"
