1bc8b6228a527821118e41b964b38a2737f741a7
Nikita Kovaljov <nikita.kovaljov@gmail.com>
2021-11-29 13:36:52 +0200

    asd

--- app.py
+++ app.py
@@ -25,8 +25,7 @@ def nmap():
             os.chdir(path)
             
             result = subprocess.check_output("./nmap -v -p 22 -oX - scanme.nmap.org", shell=True)
-            print("test")
-            return("OK")
+            return(result.text)
             
     else:
         return render_template('/templates/nmap.html')
