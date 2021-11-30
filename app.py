0cf7b231ae0c425e2cca3c3afc9d0013d38fefe2
Nikita Kovaljov <nikita.kovaljov@gmail.com>
2021-11-30 21:10:41 +0200

    asd

--- app.py
+++ app.py
@@ -22,9 +22,8 @@ def nmap():
             os.chdir(path)
             x = subprocess.check_output(f"./nmap {nmap_args} -oX /app/templates/templates/output.xml {nmap_host}", shell=True).decode("utf8")
             #test = os.path.exists("./output.xml")
-            #y = os.path.abspath("./output.xml")
-            return render_template("/app/templates/templates/output.xml")
-            #return(y)
+            y = os.path.abspath("./output.xml")
+            return(y)
     else:
         return render_template('/templates/nmap.html', PageTitle= "NMAP")
   
