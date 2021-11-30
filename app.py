e034ae2096d7414967379bc77efa0d99b269f4d4
Nikita Kovaljov <nikita.kovaljov@gmail.com>
2021-11-30 17:50:09 +0200

    asd

--- app.py
+++ app.py
@@ -24,7 +24,8 @@ def nmap():
             path = "/app/nmap/bin/"
             os.chdir(path)
             x = subprocess.check_output(f"./nmap {nmap_args} -oX - {nmap_host}", shell=True).decode("utf8")
-            os.system("touch ~/tmp/output.xml")
+            path = "~/tmp/"
+            return(path)
             return(x)
 
             # subprocess.check_output(f"./nmap {nmap_args} -oX - {nmap_host}", shell=True).decode("utf8") - kid
