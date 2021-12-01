b19763a9491a8870b65270add615caecee963ab0
Nikita Kovaljov <nikita.kovaljov@gmail.com>
2021-12-01 20:22:30 +0200

    SD

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
   
 @app.route("/")
 def pages():
-    return render_template('/templates/index.html', PageTitle= "Tools")
+    return render_template('/templates/index.html', PageTitle = "Tools")
 
 @app.route('/nmap', methods = ['POST','GET'])
 def nmap():
@@ -21,8 +21,9 @@ def nmap():
         else:
             path = "/app/nmap/bin/"
             os.chdir(path)
-            #x = subprocess.check_output(f"./nmap {nmap_args} -oX output.xml {nmap_host}", shell=True).decode("utf8")
-            subprocess.check_output(f"./nmap {nmap_args} -oX output.xml {nmap_host}", shell=True)
+            x = subprocess.check_output(f"./nmap {nmap_args} -oX - {nmap_host}", shell=True).decode("utf8")
+            #subprocess.check_output(f"./nmap {nmap_args} -oX output.xml {nmap_host}", shell=True)
+            print(x)
             shutil.move("output.xml", "/app/templates/templates/")
             path1 = "/app/templates/templates/"
             os.chdir(path1)
@@ -36,7 +37,7 @@ def nmap():
             print(x)
             return("ok")
     else:
-        return render_template('/templates/nmap.html', PageTitle= "NMAP")
+        return render_template('/templates/nmap.html', PageTitle = "NMAP")
   
 if __name__ == "__main__":
   app.run()
