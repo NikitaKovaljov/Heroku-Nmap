322c7907a881bde3593fa0545ee39406478804d7
Nikita Kovaljov <nikita.kovaljov@gmail.com>
2021-12-02 20:04:26 +0200

    asd

--- app.py
+++ app.py
@@ -6,20 +6,22 @@ import subprocess
 import shutil
   
 app = Flask(__name__)
-  
+
+file_name = 1
+new_file_name = 1
+
 @app.route("/")
 def pages():
     return render_template('/templates/index.html', PageTitle = "Tools")
 
 @app.route('/nmap', methods = ['POST','GET'])
 def nmap():
+    
     if request.method == "POST":
         nmap_args = request.form.get("args")
         nmap_host = request.form.get("host")
         path = "/app/nmap/bin/"
         path1 = "/app/templates/templates/"
-        file_name +=1
-        new_file_name +=1
 
         if len(nmap_host) == 0 and len(nmap_args) == 0:
             return "Problems with Request"
@@ -41,11 +43,13 @@ def nmap():
                 pass
             else:
                 shutil.move(f"new{new_file_name}.html", "/app/templates/templates/")
+                
             os.chdir(path1)
             final = new_file_name
-            print(file_name)
+            file_name += 1
+            new_file_name += 1
+            #print(file_name)
             return send_from_directory('/app/templates/templates',f'new{final}.html')
-
     else:
         return render_template('/templates/nmap.html', PageTitle = "NMAP")
   
