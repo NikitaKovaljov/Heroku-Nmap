c690ee6a55ef4cbd9a6ce8fab33cd0f13ba674ab
Nikita Kovaljov <nikita.kovaljov@gmail.com>
2021-12-02 17:15:46 +0200

    asd

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template, request, send_from_directory
+from os.path import exists
 import os
 import json
 import subprocess
@@ -15,19 +16,37 @@ def nmap():
     if request.method == "POST":
         nmap_args = request.form.get("args")
         nmap_host = request.form.get("host")
+        path = "/app/nmap/bin/"
+        path1 = "/app/templates/templates/"
+        file_name = 1
+        new_file_name = 1
 
         if len(nmap_host) == 0 and len(nmap_args) == 0:
             return "Problems with Request"
         else:
-            path = "/app/nmap/bin/"
             os.chdir(path)
-            os.system("wget https://svn.nmap.org/nmap/docs/nmap.xsl")
-            subprocess.check_output(f"./nmap {nmap_args} -oX output.xml --stylesheet nmap.xsl {nmap_host}", shell=True)
-            os.system("xsltproc output.xml -o new.html")     
-            shutil.move("new.html", "/app/templates/templates/")
-            path1 = "/app/templates/templates/"
+            if os.path.exists("nmap.xsl"):
+                pass
+            else:
+                os.system("wget https://svn.nmap.org/nmap/docs/nmap.xsl")
+            if os.path.exists(f"output{file_name}.xml"):
+                pass
+            else:
+                subprocess.check_output(f"./nmap {nmap_args} -oX output{file_name}.xml --stylesheet nmap.xsl {nmap_host}", shell=True)
+            if os.path.exists(f"new{new_file_name}.html"):
+                pass
+            else:
+                os.system(f"xsltproc output{file_name}.xml -o new{new_file_name}.html")    
+            if os.path.exists(f"/app/templates/templates/new{new_file_name}.html"):
+                pass
+            else:
+                shutil.move(f"new{new_file_name}.html", "/app/templates/templates/")
             os.chdir(path1)
-            return send_from_directory('/app/templates/templates','new.html')
+            final = new_file_name
+            new_file_name += 1
+            file_name += 1
+            return send_from_directory('/app/templates/templates',f'new{final}.html')
+
     else:
         return render_template('/templates/nmap.html', PageTitle = "NMAP")
   
